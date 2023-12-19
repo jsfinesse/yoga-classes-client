86e9547a0555e8a424009d90ad26a6a20e10892c
Prithu Singh <prithusingh0327@gmail.com>
2023-12-19 22:08:05 +0530

    Added env

--- components/AdmissionForm.jsx
+++ components/AdmissionForm.jsx
@@ -34,8 +34,11 @@ const AdmissionForm = () => {
         }
 
         try {
+            const API_BASE_URL =
+                process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+
             const response = await axios.post(
-                "http://localhost:5000/api/enroll",
+                `${API_BASE_URL}/api/enroll`,
                 formData
             );
             console.log(response.data);
