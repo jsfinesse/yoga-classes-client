dc171980f57947ba8b0177fd363cc1da001f1485
Prithu Singh <prithusingh0327@gmail.com>
2023-12-19 13:37:52 +0530

    Added email field, new css

--- components/AdmissionForm.jsx
+++ components/AdmissionForm.jsx
@@ -5,7 +5,8 @@ const AdmissionForm = () => {
     const [formData, setFormData] = useState({
         name: "",
         age: "",
-        selectedBatch: "",
+        email: "",
+        selectedSlot: "",
     });
 
     const handleChange = (e) => {
@@ -16,7 +17,10 @@ const AdmissionForm = () => {
         e.preventDefault();
 
         try {
-            const response = await axios.post("/api/submitForm", formData);
+            const response = await axios.post(
+                "http://localhost:5000/api/enroll",
+                formData
+            );
             console.log(response.data);
         } catch (error) {
             console.error("Error submitting form:", error);
@@ -32,7 +36,6 @@ const AdmissionForm = () => {
                 value={formData.name}
                 onChange={handleChange}
             />
-
             <label htmlFor="age">Age:</label>
             <input
                 type="number"
@@ -40,11 +43,17 @@ const AdmissionForm = () => {
                 value={formData.age}
                 onChange={handleChange}
             />
-
-            <label htmlFor="selectedBatch">Selected Batch:</label>
+            <label htmlFor="email">Email:</label>
+            <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+            />
+            <label htmlFor="selectedSlot">Selected Slot:</label>
             <select
-                name="selectedBatch"
-                value={formData.selectedBatch}
+                name="selectedSlot"
+                value={formData.selectedSlot}
                 onChange={handleChange}
             >
                 <option value="6-7AM">6-7AM</option>
@@ -52,7 +61,6 @@ const AdmissionForm = () => {
                 <option value="8-9AM">8-9AM</option>
                 <option value="5-6PM">5-6PM</option>
             </select>
-
             <button type="submit">Submit</button>
         </form>
     );
